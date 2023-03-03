c4a33d99491163d117fa9a4ada27ed46042f0edc
Vu Huy Hoang <uchihavuhuyhoang@gmail.com>
2023-03-03 15:01:01 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
             ),
       ),
       routes: {
-        '/': (context) => HomeScreen(),
+        '/': (context) => const HomeScreen(),
       },
     );
   }
