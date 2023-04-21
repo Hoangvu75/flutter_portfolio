9c865f97725a53af4c1f50aac99c54863535bd87
Vu Huy Hoang <uchihavuhuyhoang@gmail.com>
2023-04-21 15:26:36 +0700

    update

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/animation/my_animated_divider.dart';
-import 'package:portfolio/animation/my_animated_slide_fade_from_left.dart';
-import 'package:portfolio/generated/constants.dart';
-import 'package:portfolio/responsive.dart';
 import 'package:portfolio/screen/home/components/about_me.dart';
-import 'package:timelines/timelines.dart';
 
-import '../../animation/my_animated_slide_fade_from_right.dart';
-import '../../generated/assets.dart';
 import '../main/main_screen.dart';
 import 'components/footer.dart';
 import 'components/high_light_group.dart';
